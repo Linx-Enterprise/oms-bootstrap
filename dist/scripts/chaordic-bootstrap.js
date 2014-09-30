2ac9b56b13abb85126de15baf5752360de674d2c
Guilherme Pacheco <guilherme.f.pacheco@hotmail.com>
2014-09-30 15:59:34 -0300

    Fixed product selector animation on no-sidebar pages

--- dist/scripts/chaordic-bootstrap.js
+++ dist/scripts/chaordic-bootstrap.js
@@ -4328,13 +4328,19 @@ $(document)
 
 $(document)
     // Add slidedown animation to dropdown
-    .on('show.bs.dropdown', '.sidebar-brand', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    .on('show.bs.dropdown', '.sidebar-brand, .product-selector > .btn-group', function() {
+        var jqThis = $(this);
+        jqThis.addClass('animating').find('.dropdown-menu').first().stop(true, true).slideDown(function() {
+            jqThis.removeClass('animating');
+        });
     })
 
     // Add slideup animation to dropdown
-    .on('hide.bs.dropdown', '.sidebar-brand', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    .on('hide.bs.dropdown', '.sidebar-brand, .product-selector > .btn-group', function() {
+        var jqThis = $(this);
+        jqThis.addClass('animating').find('.dropdown-menu').first().stop(true, true).slideUp(function() {
+            jqThis.removeClass('animating');
+        });
     });
 
 /* ==================
