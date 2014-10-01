07b1caa57f9a50cb773a9e012ab19559c892dae5
Guilherme Pacheco <guilherme.f.pacheco@hotmail.com>
2014-10-01 14:16:22 -0300

    Close product selector on mouse leave

--- src/scripts/product-selector.js
+++ src/scripts/product-selector.js
@@ -20,4 +20,9 @@ $(document)
         jqThis.addClass('animating').find('.dropdown-menu').first().stop(true, true).slideUp(function() {
             jqThis.removeClass('animating');
         });
+    })
+
+    // Close dropdown on mouse leave
+    .on('mouseleave', '.sidebar-brand .dropdown-menu, .product-selector > .btn-group .dropdown-menu', function() {
+        $(this).dropdown('toggle');
     });
